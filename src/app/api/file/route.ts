0bf3553dfc2742d038dc11026f9e1cfd819c9d1d
ThomasMoras <moras.thomas@gmail.com>
2025-04-02 16:27:11 -0400

    update gitingore & fix build

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -6,8 +6,7 @@ import { join } from "path";
 export async function POST(request: Request) {
   const formData = await request.formData();
   const file = formData.get("file") as File;
-  var folder = formData.get("folder") as string;
-  folder = folder.toLocaleLowerCase();
+  const folder = (formData.get("folder") as string).toLocaleLowerCase();
 
   if (!file) {
     return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
