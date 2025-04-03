932e9a07863ddb67430e2a0ca1dbabd6b1bac435
ThomasMoras <moras.thomas@gmail.com>
2025-04-03 17:59:25 -0400

    fix flavicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,30 +48,14 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico", sizes: "any" },
-      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon.svg", type: "image/svg+xml" }, // Optionnel
+      { url: "/favicon-32x32.png", type: "image/png", sizes: "32x32" },
     ],
     apple: "/apple-touch-icon.png",
     other: [
       {
-        rel: "icon",
-        type: "image/png",
-        sizes: "32x32",
-        url: "/favicon-32x32.png",
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "16x16",
-        url: "/favicon-16x16.png",
-      },
-      {
-        rel: "apple-touch-icon",
-        sizes: "180x180",
-        url: "/apple-touch-icon.png",
-      },
-      {
         rel: "manifest",
-        url: "/site.webmanifest",
+        url: "/site.webmanifest", // Uniquement si vous avez les PNG 192/512
       },
     ],
   },
